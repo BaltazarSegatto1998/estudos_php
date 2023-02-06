9d6ade1bfbffff088e23b40f2d91fc622c857d17
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-06 19:53:09 -0300

    Create switch.php

---
+++ lógica/switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$tipo = 'video';
+
+
+switch($tipo) {
+	case 'foto':
+		echo 'Exibindo Foto';
+	break;
+		case 'video':
+		echo 'Exibindo Video';
+	break;
+		case 'texto':
+		echo 'Exibindo Texto';
+	break;
+}
+
