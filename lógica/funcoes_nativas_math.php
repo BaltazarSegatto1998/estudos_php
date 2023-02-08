78e25d7450399650f63aab86a871bdf575462c74
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-08 15:11:26 -0300

    Create funcoes_nativas_math.php

---
+++ lógica/funcoes_nativas_math.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$numero = 12.12345;
+$aleatorio = rand(3,10);
+$lista = [1,2,3,6,7,8];
+	
+
+echo round($numero, 3)."<br/>";
+echo $aleatorio."<br/>";
+echo max($lista)."<br/>";
+echo min($lista);
