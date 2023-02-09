a3503eefaa16b5ba853d2b910a64017384d657a4
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-09 11:20:07 -0300

    Create dados.php

---
+++ cookie_form_dele/dados.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$nome = filter_input(INPUT_GET, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_GET, 'email', FILTER_VALIDATE_EMAIL);
+$idade = filter_input(INPUT_GET, 'idade', FILTER_VALIDATE_INT);
+
+
+
+if ($nome && $email && $idade) {
+    $expiracao = time() + (86400 * 30);
+    setcookie('nome', $nome, $expiracao);
+
+    echo 'NOME: ' . $nome . '<br/>';
+    echo 'EMAIL: ' . $email . '<br/>';
+    echo 'IDADE: ' . $idade . '<br/>';
+
+} else {
+
+    $_SESSION['aviso'] = 'Preencha os itens corretamente!';
+
+    header("Location:index.php");
+    exit;
+}
+
+
+
+?>
