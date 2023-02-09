ab25b4b103e9ef4f6d10bbcdc3fd60e029ccc4f5
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-09 12:52:20 -0300

    Create login.php

---
+++ login_session_form/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form method="GET" action="dados.php">
+    <h1>Qual é o seu nome? </h1> <br>
+
+    <label for="">Nome
+    <input type="text" name="nome" id=""> 
+    </label>
+
+        <input type="submit" value="Enviar">
+
+</form>
+    
+</body>
+</html>
