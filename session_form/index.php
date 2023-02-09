2cb23e8580cb7b03934432f21a44348edd90027c
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-09 10:20:54 -0300

    Create index.php

---
+++ session_form/index.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+if ($_SESSION['aviso']) {
+    echo $_SESSION['aviso'];
+    $_SESSION['aviso'] = '';
+
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form method="GET" action="dados.php">
+
+    <label for="">Nome
+    <input type="text" name="nome" id="">
+    </label>
+
+
+    <br>
+    <br>
+
+    <label for="">Email
+    <input type="text" name="email" id="">
+    </label>
+
+
+    <br>
+    <br>
+
+
+    <label for="">Idade
+    <input type="text" name="idade" id="">
+    </label>
+
+        <input type="submit" value="Enviar">
+
+</form>
+    
+</body>
+</html>
