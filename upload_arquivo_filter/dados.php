72633c0b123db8375ebbef1033b2aec983d26a45
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-09 21:52:08 -0300

    Update dados.php

--- upload_arquivo_filter/dados.php
+++ upload_arquivo_filter/dados.php
@@ -1,7 +1,9 @@
 <?php
 print_r($_FILES);
 
-if(in_array($_FILES['arquivo']['type'], array('image/jpeg', 'image/jpg', 'image/png'))) {
+$permidos = ['image/jpeg', 'image/jpg', 'image/png'];
+
+if(in_array($_FILES['arquivo']['type'], $permitidos)) {
     $nome = md5(time() . rand(0, 10000)).'.jpg';
     move_uploaded_file($_FILES['arquivo']['tmp_name'], 'arquivos/'.$nome);
     echo 'Arquivo salvo';
