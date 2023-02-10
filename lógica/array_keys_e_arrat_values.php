18cb6abd25a043500cc39df7cd4b0f4a1c52ea23
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-10 13:28:28 -0300

    Create array_keys_e_arrat_values.php

---
+++ lógica/array_keys_e_arrat_values.php
@@ -0,0 +1,12 @@
+<?php
+$array = [
+    'nome' => 'Sidnei',
+    'idade' => 25,
+    'empresa' => 'rosh',
+    'profissao' => 'Desenvolvedor'
+
+];
+
+$chaves = array_keys($array);
+
+$valores = array_values($array);
