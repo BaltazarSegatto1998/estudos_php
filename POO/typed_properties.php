d2fd868653544c6a65e85d92bd2478a10904b28c
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-11 15:57:52 -0300

    Create typed_properties.php

---
+++ POO/typed_properties.php
@@ -0,0 +1,34 @@
+<?php
+
+// Classe define as propriedades(caracteristica) de um objeto
+// Classes possuem dois itens, métodos e propriedades
+
+
+
+class Post {
+    public int $likes = 0;
+    public array $coments = [];
+    public string $author;
+
+    public function aumentarLike() {
+        $this->likes++;
+    }
+}
+
+// Objeto é um item real criado a partir de uma modelo(classe).
+$post1 = new Post();
+$post1->aumentarLike();
+
+
+$post2 = new Post();
+$post2->aumentarLike();
+
+
+//Propriedades são as caracteristicas que uma classe irá ter
+
+//Métodos são funções executadas dentro do objeto 
+
+echo "POST 1: ".$post1->likes."<br>";
+
+echo "POST 2: ".$post2->likes."<br>";
+
