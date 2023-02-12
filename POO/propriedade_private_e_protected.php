29cbd0607f3985ba5c15ff2bf5087b4121741ef0
Sidnei Baltazar Segatto Junior <70817094+BaltazarSegatto1998@users.noreply.github.com>
2023-02-12 12:12:48 -0300

    Create propriedade_private_e_protected.php

---
+++ POO/propriedade_private_e_protected.php
@@ -0,0 +1,55 @@
+<?php
+
+//Private só pode ser alterada pela classe que a criou;
+//Protect pode ser alterada por classes que herdaram seus atributos
+
+class Post {
+    private int $id;
+    private int $likes = 0;
+
+    protected function setId($i){
+        $this->id = $i;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setLikes($i) {
+        $this->likes = $i;
+    }
+
+    public function getLikes() {
+        return $this->likes;
+    }
+
+}
+
+
+// A classe foto herda tudo que tem a classe Post
+
+class Foto extends Post {
+    private $url;
+
+
+
+    public function __construct($id){
+            $this->setId($id);
+    }
+
+    public function setUrl($u) {
+        $this->url = $u;
+    }
+
+    public function getUrl() {
+        return $this->url;
+    }
+}
+
+
+$foto = new Foto(20);
+$foto->setLikes(12);
+$foto->setUrl('www.minhaurl.com');
+
+
+echo "Foto: #".$foto->getId()." - ".$foto->getLikes()." likes -  ".$foto->getUrl();
